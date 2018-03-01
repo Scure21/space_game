9c3fc86cb3b93de8cf48f12a00dc597cc4f6105f
Stephanie Cure <stephanie.cure@willowtreeapps.com>
2018-03-01 15:34:51 -0500

    Add correct dependencies and compile scss

--- src/index.jsx
+++ src/index.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import Main from "./components/Main.jsx";
+require("./styles/index.scss");
 
 ReactDOM.render(<Main />, document.getElementById("app"));
