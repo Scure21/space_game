a589884f46c6014a67967dad7018b76edd8c99e3
Stephanie Cure <stephanie.cure@willowtreeapps.com>
2018-03-06 13:45:32 -0500

    add id to html

--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -1,15 +1,21 @@
 import React, { Component } from "react";
-import { Canvas } from "./canvas";
+import { start } from "./canvas";
 
 export default class App extends Component {
   constructor(props) {
     super(props);
   }
+  componentDidMount() {
+    start();
+  }
+
   render() {
     return (
       <section className="container">
         <div className="header">Header</div>
-        <div className="game">Game</div>
+        <div className="game" id="game">
+          Game
+        </div>
         <div className="footer">
           Brought to you with{" "}
           <img src="assets/images/heart.png" className="footer-image" /> by
--- src/components/canvas.js
+++ src/components/canvas.js
@@ -1,31 +1,31 @@
-// import "phaser";
+import "phaser";
 
-// var config = {
-//   type: Phaser.AUTO,
-//   parent: "phaser-example",
-//   width: 800,
-//   height: 600,
-//   scene: {
-//     preload: preload,
-//     create: create
-//   }
-// };
+var config = {
+  type: Phaser.AUTO,
+  parent: "game",
+  width: 800,
+  height: 600,
+  scene: {
+    preload: preload,
+    create: create
+  }
+};
 
-// const game = new Phaser.Game(config);
+export const start = () => new Phaser.Game(config);
 
-// function preload() {
-//   this.load.image("logo", "assets/images/logo.png");
-// }
+function preload() {
+  this.load.image("logo", "assets/images/logo.png");
+}
 
-// function create() {
-//   var logo = this.add.image(400, 150, "logo");
+function create() {
+  var logo = this.add.image(400, 150, "logo");
 
-//   this.tweens.add({
-//     targets: logo,
-//     y: 450,
-//     duration: 2000,
-//     ease: "Power2",
-//     yoyo: true,
-//     loop: -1
-//   });
-// }
+  this.tweens.add({
+    targets: logo,
+    y: 450,
+    duration: 2000,
+    ease: "Power2",
+    yoyo: true,
+    loop: -1
+  });
+}
